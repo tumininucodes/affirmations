a1cdec0f9f569de55e77bec1841a49e5e8309d07
Tumi <oluwatumininuojo25@gmail.com>
2021-01-20 05:02:43 +0100

    layout optimisations

---
+++ app/src/main/java/com/kith/affirmations/adapter/ItemAdapters.kt
@@ -0,0 +1,38 @@
+package com.kith.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.kith.affirmations.R
+import com.kith.affirmations.model.Affirmation
+
+class ItemAdapter(private  val context: Context, private val dataset: List<Affirmation>)
+    : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
+    }
+
+    override fun getItemCount(): Int = dataset.size
+
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
+    }
+
+
+}
---
+++ app/src/main/java/com/kith/affirmations/ui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.kith.affirmations.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kith.affirmations.adapter.ItemAdapter
+import com.kith.affirmations.data.DataSource
+import com.kith.affirmations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataSet = DataSource().loadAffirmations()
+
+        val recyclerView = binding.recyclerView
+
+        recyclerView.adapter = ItemAdapter(this, myDataSet)
+
+        recyclerView.setHasFixedSize(true)
+    }
+}
